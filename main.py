d3383394c42ddbfc566d0b3d4f67d877c73bf935
mikhail <mmdauvalder@gmail.com>
2026-03-31 19:15:49 +0600

    first commit

--- main.py
+++ main.py
@@ -11,59 +11,55 @@ app = FastAPI()
 SessionDep = Annotated[AsyncSession, Depends(get_session)]
 
 
-#ЗАКОММИТИЛ УЖЕ С ГОТОВОЙ
-@app.post('/database_startup',
-          tags=['База данных'],
-          summary='Создание базы данных перед использованием')
+# ЗАКОММИТИЛ УЖЕ С ГОТОВОЙ
+@app.post(
+    "/database_startup",
+    tags=["База данных"],
+    summary="Создание базы данных перед использованием",
+)
 async def create_database():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-    return {'База данных создана': True}
+    return {"База данных создана": True}
 
 
-
-@app.get('/recipes',
-         tags=['Рецепты'],
-         summary='Получить список всех рецептов')
+@app.get("/recipes", tags=["Рецепты"], summary="Получить список всех рецептов")
 async def get_recipe(session: SessionDep):
-    query = select(RecipeModel.title, RecipeModel.views,
-                   RecipeModel.cooking_time).order_by(desc(RecipeModel.views),
-                                                      asc(RecipeModel.cooking_time))
+    query = select(
+        RecipeModel.title, RecipeModel.views, RecipeModel.cooking_time
+    ).order_by(desc(RecipeModel.views), asc(RecipeModel.cooking_time))
     result = await session.execute(query)
     return result.mappings().all()
 
 
-
-@app.get('/recipes/{recipe_id}',
-         tags=['Рецепты'],
-         summary='Получить один конкретный рецепт')
+@app.get(
+    "/recipes/{recipe_id}", tags=["Рецепты"], summary="Получить один конкретный рецепт"
+)
 async def get_spec_recipe(recipe_id, session: SessionDep):
     query = select(RecipeModel).where(RecipeModel.id == recipe_id)
-    counter_query = update(RecipeModel).values(views=RecipeModel.views + 1).where(RecipeModel.id == recipe_id)
+    counter_query = (
+        update(RecipeModel)
+        .values(views=RecipeModel.views + 1)
+        .where(RecipeModel.id == recipe_id)
+    )
     await session.execute(counter_query)
     await session.commit()
     result = await session.execute(query)
     return result.scalar()
 
 
-@app.post('/recipes',
-          tags=['Рецепты'],
-          summary='Добавить новый рецепт')
+@app.post("/recipes", tags=["Рецепты"], summary="Добавить новый рецепт")
 async def get_recipes(data: RecipeSchema, session: SessionDep):
     new_recipe = RecipeModel(
         title=data.title,
         cooking_time=data.cooking_time,
         ingredients=data.ingredients,
-        description=data.description
+        description=data.description,
     )
     session.add(new_recipe)
     await session.commit()
-    return {'message' : 'Рецепт добавлен'}
+    return {"message": "Рецепт добавлен"}
 
 
-
-
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
